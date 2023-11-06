88124c58111dae1d421bf1668ee8ed7ce6036589
Darkbuilder646 <theolejeune.tl@gmail.com>
2023-11-06 13:53:07 +0100

    Update main for load model

--- Game_of_Life/Main.java
+++ Game_of_Life/Main.java
@@ -7,35 +7,47 @@ import Game_of_Life.Model.OscillatorsModel;
 import Game_of_Life.Model.SmallModel;
 import Game_of_Life.Model.StillLifeModel;
 
-
 public class Main {
     public static void main(String[] args) {
         GameOfLife gameOfLife = new GameOfLife(35, 50);
         boolean isGameRunning = true;
+        boolean customLoad = true; //* Set to false for load preset model
 
         LoadModel loader = new LoadModel();
-        int[][] customModel = new int[10][10];
-        // loader.setModelToLoad(SmallModel.Small2); // * Choose here the model to load
-         
-        try {
-            loader.setRandomCellToLoad(customModel, (float) 0.35);
-        } catch (ModelException e) {
-            System.out.println("Model size error : " + e.getMessage());
-            isGameRunning = false;
-        }
+        
 
-        try {
-            loader.load(gameOfLife);
-        } catch (ModelException e) {
-            System.out.println("Model size error : " + e.getMessage());
-            isGameRunning = false;
+        if (customLoad) {
+            int[][] customModel = new int[10][10];
+
+            try {
+                loader.setRandomCellToLoad(customModel, (float) 0.35);
+            } catch (ModelException e) {
+                System.out.println("Model size error : " + e.getMessage());
+                isGameRunning = false;
+            }
+
+            try {
+                loader.load(gameOfLife);
+            } catch (ModelException e) {
+                System.out.println("Model size error : " + e.getMessage());
+                isGameRunning = false;
+            }
+        } else {
+            loader.setModelToLoad(OscillatorsModel.pulsar); // * Choose here the model to load
+
+            try {
+                loader.load(gameOfLife);
+            } catch (ModelException e) {
+                System.out.println("Model size error : " + e.getMessage());
+                isGameRunning = false;
+            }
         }
 
         while (isGameRunning) {
             gameOfLife.display();
 
             try {
-                Thread.sleep(1000); //? Speed of generation 10000 => 10s
+                Thread.sleep(100); // ? Speed of generation 10000 => 10s
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
